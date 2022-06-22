3e0a578e525cb8d6b2a46193349a247566530ae9
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-22 16:45:54 +0200

    corretto

--- palindromo.js
+++ palindromo.js
@@ -1,5 +1,14 @@
+// function isPalindrome(word) {
+//     return word == word.split('').reverse().join('');
+// }
+
 function isPalindrome(word) {
-    return word == word.split('').reverse().join('');
+    for (let i = 0; i < word.length / 2; i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 const userWord = prompt('inserire una parola');
