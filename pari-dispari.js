353dcea49ab4316f8e3e0a491c008465110e78b8
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-22 15:02:32 +0200

    pari dispari

--- pari-dispari.js
+++ pari-dispari.js
@@ -11,7 +11,7 @@ while (choiceNotValid) {
 }
 
 while (numberNotValid) {
-    userNumber = prompt('inserire un numero da 1 a 5');
+    userNumber = parseInt(prompt('inserire un numero da 1 a 5'));
     if (userNumber >= 1 && userNumber <= 5) {
         numberNotValid = false;
     }
@@ -19,3 +19,33 @@ while (numberNotValid) {
 
 let aiNumber = 0;
 
+function randomAiNumber() {
+    return Math.floor((Math.random() * 5) + 1);
+}
+
+aiNumber = randomAiNumber();
+
+console.log(`user: ${userNumber}`);
+console.log(`ai: ${aiNumber}`);
+
+const sum = userNumber + aiNumber;
+
+console.log(`somma: ${sum}`);
+
+let isEven = 0;
+
+function evenControl(inputNumber) {
+    if (inputNumber % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+isEven = evenControl(sum);
+
+if ((isEven && userChoice == 'pari') || (!isEven && userChoice == 'dispari')) {
+    console.log('complimenti hai vinto!');
+} else {
+    console.log('mi spiace hai perso...');
+}
